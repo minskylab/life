37645b53e5b1893f6e196b600ac6a6f815a8ee22
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-10-30 01:14:34 -0500

    fix (cli): Workin in progress

--- generic-gen.go
+++ generic-gen.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"strings"
@@ -15,7 +15,7 @@ import (
 
 type genTypeExtension string
 
-const graphqlGen genTypeExtension = ".graphql"
+const graphqlGen genTypeExtension = ".graphqls"
 const entGen genTypeExtension = ".go"
 
 func generateTemplate(entity Entity, tmpl *template.Template, out io.Writer) error {
@@ -27,9 +27,23 @@ func generateSchemaDir(where string, entities []Entity, tmpl *template.Template,
 
 	_ = os.MkdirAll(where, os.ModePerm)
 
+	scalarsPath := path.Join(where, "scalars"+string(ext))
+	var scalarsFile *os.File
+
 	for _, ent := range entities {
 		kind := entityKind(ent)
+
 		if kind == ScalarKind {
+			if ext == graphqlGen {
+				if scalarsFile == nil {
+					scalarsFile, _ = os.OpenFile(scalarsPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+					defer scalarsFile.Close()
+				}
+
+				if _, err := scalarsFile.WriteString(fmt.Sprintf("scalar %s\n", ent.Name)); err != nil {
+					return errors.WithStack(err)
+				}
+			}
 			continue
 		}
 
@@ -56,17 +70,24 @@ func generateSchemaDir(where string, entities []Entity, tmpl *template.Template,
 func executeEntGenerator(where string, entities []Entity) error {
 	tmpl, err := template.ParseFiles("ent.go.tmpl")
 	if err != nil {
-		log.Fatal(err)
+		return errors.WithStack(err)
 	}
 
 	return generateSchemaDir(where, entities, tmpl, entGen)
 }
 
 func executeGraphQLGenerator(where string, entities []Entity) error {
-	tmpl, err := template.ParseFiles("gql.graphql.tmpl")
+	tmpl, err := template.ParseFiles("gql.graphqls.tmpl")
 	if err != nil {
-		log.Fatal(err)
+		return errors.WithStack(err)
 	}
 
-	return generateSchemaDir(where, entities, tmpl, graphqlGen)
+	if err := generateSchemaDir(where, entities, tmpl, graphqlGen); err != nil {
+		return errors.WithStack(err)
+	}
+
+	basics := path.Join(where, "operations", "basic.graphqls")
+	if !fileExists(basics) {
+
+	}
 }
